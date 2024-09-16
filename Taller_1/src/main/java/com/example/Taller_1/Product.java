ab069279ab5a4b0187d6aa343272d83914d4cd3e
RubioJuan <152001380+RubioJuan@users.noreply.github.com>
2024-09-16 13:34:28 -0500

    refactor: :bug: Arreglando errores
    JPA no tenia que ir

---
+++ Taller_1/src/main/java/com/example/Taller_1/Product.java
@@ -0,0 +1,49 @@
+package com.example.Taller_1;
+// Model
+public class Product {
+    private int id;
+    private String name;
+    private double price;
+
+    // Constructor
+    public Product(int id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    // Getters y setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    // Metodo toString para representar el producto
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
