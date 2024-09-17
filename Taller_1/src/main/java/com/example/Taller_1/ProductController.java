701856142cec6fd178c868234d1d9fd6b701847b
RubioJuan <152001380+RubioJuan@users.noreply.github.com>
2024-09-17 06:52:24 -0500

    refactor: :art: Mejorando el controller
    Agregando formato Json

--- Taller_1/src/main/java/com/example/Taller_1/ProductController.java
+++ Taller_1/src/main/java/com/example/Taller_1/ProductController.java
@@ -1,5 +1,7 @@
 package com.example.Taller_1;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductController {
 
     @GetMapping
-    public String getAllProducts() {
-        // Retorna una lista de productos o un mensaje
-        return "Listo, salió pa pintura";
+    public List<Product> getAllProducts() {
+        // Crear una lista de productos
+        List<Product> products = new ArrayList<>();
+        products.add(new Product(1, "Product A", 10.99));
+        products.add(new Product(2, "Product B", 12.99));
+        products.add(new Product(3, "Product C", 8.99));
+
+        // Retornar la lista de productos
+        return products;
     }
 }
