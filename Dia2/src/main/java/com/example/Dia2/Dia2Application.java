7e37209a8d38c4aa6e245fe7bc7f0e71af215fe4
RubioJuan <152001380+RubioJuan@users.noreply.github.com>
2024-09-17 08:40:26 -0500

    feat: :sparkles: Día 2
    Dependencías y java beans

---
+++ Dia2/src/main/java/com/example/Dia2/Dia2Application.java
@@ -0,0 +1,12 @@
+package com.example.Dia2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dia2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dia2Application.class, args);
+	}
+}
---
+++ Dia2/src/test/java/com/example/Dia2/Dia2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Dia2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dia2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
